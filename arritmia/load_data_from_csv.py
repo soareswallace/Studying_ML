22266105703e3585191f60a75787f5b88f032390
Wallace Soares <soareswallace@icloud.com>
2018-10-20 21:17:18 -0300

    fisrt commit

---
+++ arritmia/load_data_from_csv.py
@@ -0,0 +1,6 @@
+import pandas
+
+def load_data_from_csv(path, column_names):
+    url = path
+    names = columns
+    data = pandas
---
+++ load_csv.py
@@ -0,0 +1,14 @@
+# Load CSV using Pandas from URL
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+print(data.shape)
+
+# Statistical Summary
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+description = data.describe()
+print(description)
